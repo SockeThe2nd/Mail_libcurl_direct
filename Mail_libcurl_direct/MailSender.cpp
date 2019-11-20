7684d2ee2ad87fbd874706e5baeb7f8a2f16744d
Roland Paltz <roland.paltz@kraemer-it.de>
2019-11-20 20:29:21 +0100

    Add init

---
+++ Mail_libcurl_direct/MailSender.cpp
@@ -0,0 +1,159 @@
+﻿#include "pch.h"
+#include "MailSender.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#include <curl/curl.h>
+
+/// <summary>
+/// MailSender-Klasse
+/// 
+/// Regelt den Versandt von Mails ueber den angegebenen Mailserver
+/// </summary>
+/// <param name="server">Mailserver. Format: smtp://serveradresse:port</param>
+/// <param name="user">Benutzer und Passwort. Format: user:pass</param>
+MailSender::MailSender(string server, string user)
+{
+	this->server = server;
+	this->user = user;
+
+	this->curl = curl_easy_init();
+}
+
+MailSender::~MailSender()
+{
+	if (this->curl) {
+		curl_easy_cleanup(this->curl);
+	}
+}
+
+/// <summary>
+/// Sendet eine Mail an die angegebene Adresse
+/// </summary>
+/// <param name="from">Absender-Adresse</param>
+/// <param name="to">Empfaenger-Adresse</param>
+/// <param name="subject">Betreff</param>
+/// <param name="textPlain">Mail-Inhalt als Klartext (optional)</param>
+/// <param name="textHtml">Mail-Inhalt als HTML (optional)</param>
+/// <param name="file">Pfad zum Anhang (optional)</param>
+/// <returns>CURL-Response-Code. 0 wenn erfolgreich</returns>
+int MailSender::send(string from, string to, string subject, string textPlain, string textHtml, string file)
+{
+	CURLcode res = CURLE_OK;
+	if (this->curl) {
+		// benoetigte variablen anlegen
+		struct curl_slist* headers = NULL;
+		struct curl_slist* recipients = NULL;
+		struct curl_slist* slist = NULL;
+		curl_mime* mime;
+		curl_mimepart* part;
+
+		/*
+			SMTP-Header aus Parametern zusammensetzen zusammensetzen
+
+			Siehe https://tools.ietf.org/html/rfc2076
+		*/
+		string toHeader = "To: ";
+		toHeader += to;
+		string fromHeader = "From: ";
+		fromHeader += from;
+		string subjectHeader = "Subject: ";
+		subjectHeader += subject;
+
+		//Zuvor definierte header in Headerliste einfuegen
+		headers = curl_slist_append(headers, fromHeader.c_str());
+
+
+		/*
+			Liste der Empf�nger fuellen. Muessen mit Liste der Empfaenger im header uebereinstimmen.
+		*/
+		recipients = curl_slist_append(recipients, to.c_str());
+		headers = curl_slist_append(headers, toHeader.c_str());
+
+
+		headers = curl_slist_append(headers, subjectHeader.c_str());
+
+		/*
+			Addresse oder URL des Mailservers mit Protokoll und ggf Port
+
+			smtp://serveradresse:port
+		*/
+		curl_easy_setopt(this->curl, CURLOPT_URL, this->server.c_str());
+
+		// Versuche SSL, falls nicht m�glich mache weiter ohne
+		curl_easy_setopt(this->curl, CURLOPT_USE_SSL, CURLUSESSL_TRY);
+
+		/*
+			Authentifizierung beim Server.
+			Format: user:password
+		*/
+		curl_easy_setopt(this->curl, CURLOPT_USERPWD, this->user.c_str());
+
+		// Absender angeben, muss mit Empfaenger in header uebereinstimmen
+		curl_easy_setopt(this->curl, CURLOPT_MAIL_FROM, from.c_str());
+		curl_easy_setopt(this->curl, CURLOPT_MAIL_RCPT, recipients);
+
+		// Headerliste zur anfrage hinzufuegen
+		curl_easy_setopt(this->curl, CURLOPT_HTTPHEADER, headers);
+
+		/* Mime-Nachricht initialisieren */
+		mime = curl_mime_init(this->curl);
+
+		/* Text Nachricht einfuegen. */
+		if (textPlain != "") {
+			//Neuen Abschnitt in MIME anlegen.
+			part = curl_mime_addpart(mime);
+			//Text mit suffix versehen (wird ansonsten in einigen Mailclients falsch dargestellt
+			string text = textPlain + "\r\n";
+			//Text in MIME Abschnitt einfuegen
+			curl_mime_data(part, text.c_str(), CURL_ZERO_TERMINATED);
+			//Mime-Typ setzen
+			curl_mime_type(part, "text/plain");
+			//Codierung setzen
+			curl_mime_encoder(part, "quoted-printable");
+		}
+
+		/* HTML Nachricht einfuegen. Siehe oben*/
+		if (textHtml != "") {
+			part = curl_mime_addpart(mime);
+			string html = textHtml + "\r\n";
+			curl_mime_data(part, html.c_str(), CURL_ZERO_TERMINATED);
+			curl_mime_type(part, "text/html");
+			curl_mime_encoder(part, "quoted-printable");
+		}
+
+		if (file != "") {
+			/* Anhang erstellen */
+			part = curl_mime_addpart(mime);
+			// Datei einlesen und als Anhang setzen
+			curl_mime_filedata(part, file.c_str());
+
+			// Uebertragungscodierung setzen
+			curl_mime_encoder(part, "base64");
+		}
+
+		// Mime-Nachricht zur Anfrage hinzufuegen
+		curl_easy_setopt(curl, CURLOPT_MIMEPOST, mime);
+
+		/* Anfrage senden */
+		res = curl_easy_perform(curl);
+
+		/* Auf fehler bei uebertragung pruefen; Hier ggf error werfen */
+		if (res != CURLE_OK)
+			std::cout << "CURL Fehlgeschlagen: " << curl_easy_strerror(res) << "\n";
+
+		/* Speicher wieder freigeben */
+		curl_slist_free_all(recipients);
+		curl_slist_free_all(headers);
+		curl_mime_free(mime);
+	}
+	else
+	{
+		res = CURLE_FAILED_INIT;
+	}
+
+	return res;
+}
---
+++ Mail_libcurl_direct/Mail_libcurl_direct.cpp
@@ -0,0 +1,27 @@
+﻿// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+#include "MailSender.h"
+
+using namespace std;
+
+static const string MAIL_SERVER = "smtp://smtp.gmx.net:587";
+static const string USER = "user:pass";
+
+int main(void)
+{
+	/*
+	 * Instanz der MailSender-Class erstellen.
+	 * Die Adresse des Mailservers mit Port sowie der zu verwendende Benutzer sind als Argumente angegeben.
+	 */
+	MailSender sender = MailSender(MAIL_SERVER, USER);
+
+	//Senden einer Mail. Der MailSender kann mehrfach verwendet werden sollen mehrere Mails versandt werden. 
+	sender.send("roland.paltz@gmx.net", "roland.paltz@gmx.net", "Test-Betreff", "Inhalt als text", "<b>Inhalt als HTML<\b>", "Image.png");
+	//Sollen nur text-Mails versendet werden, so kann der HTML-Parameter leer gelassen werden.
+	sender.send("roland.paltz@gmx.net", "roland.paltz@gmx.net", "Test-Betreff ohne HTML", "Inhalt als Text", "", "Image.png");
+	return sender.send("roland.paltz@gmx.net", "roland.paltz@gmx.net", "Test-Betreff ohne Inhalt");
+};
